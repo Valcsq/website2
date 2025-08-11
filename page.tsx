5eea8a325423a98123bdffa9d01f7e5118313144
Valcsq <107704239+Valcsq@users.noreply.github.com>
2025-08-11 21:09:54 +0200

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center relative">
+      <div className="absolute top-4 right-4">
+        <p className="text-white font-medium">link of a pedofile https://docs.google.com/document/d/1dwGMFOO1gLSLEvVM9iQPVnKsMui432dhXMEj2B9nbpw/edit?usp=sharing</p>
+      </div>
+
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">fuck</h1>
+        <p className="text-xl">you</p>
+      </div>
+    </div>
+  )
+}
